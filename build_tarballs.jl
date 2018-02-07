94e6b8b63485c15153cccd64ac3ba89b7b724778
Elliot Saba <staticfloat@gmail.com>
2018-02-06 16:27:44 -0800

    Use `libblas` from `OpenBLAS`

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -11,14 +11,18 @@ platforms = [
   BinaryProvider.MacOS()
 ]
 
+dependencies = [
+    "https://raw.githubusercontent.com/staticfloat/OpenBLASBuilder/master/build.jl",
+]
+
 sources = [
     "https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.8.tgz" =>
     "62c6de314220851b8f4d6898b9ae8cf0a8f1e96b68429be1161f8550bb7ddb03",
 ]
 
-script = raw"""
-cd $WORKSPACE/srcdir/Ipopt-3.12.8
-./configure --prefix=/ --host=$target
+script = """
+cd \$WORKSPACE/srcdir/Ipopt-3.12.8
+./configure --prefix=/ --with-blas-lib="-L$(libdir(Prefix("\$DESTDIR")))" --host=\$target
 make -j3
 make install
 """
@@ -27,6 +31,18 @@ products = prefix -> [
   LibraryProduct(prefix,"libipopt"),
 ]
 
-product_hashes = Dict()
-autobuild(pwd(), "Ipopt", platforms, sources, script, products, product_hashes)
-print_buildjl(product_hashes)
+# Be quiet unless we've passed `--verbose`
+verbose = "--verbose" in ARGS
+ARGS = filter!(x -> x != "--verbose", ARGS)
+
+# Choose which platforms to build for; if we've got an argument use that one,
+# otherwise default to just building all of them!
+build_platforms = platforms
+if length(ARGS) > 0
+    build_platforms = platform_key.(split(ARGS[1], ","))
+end
+info("Building for $(join(triplet.(build_platforms), ", "))")
+
+
+autobuild(pwd(), "Ipopt", build_platforms, sources, script, products;
+          dependencies=dependencies, verbose=verbose)
