5a0a80f926b6e8aa298494dd1a22dc4b2ac9bec2
Elliot Saba <staticfloat@gmail.com>
2018-02-23 16:46:35 -0800

    Add `:amplexe` as an `ExecutableProduct`

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -103,7 +103,8 @@ platforms = [
 
 # The products that we will ensure are always built
 products(prefix) = [
-    LibraryProduct(prefix,"libipopt", :libipopt),
+    LibraryProduct(prefix, "libipopt", :libipopt),
+    ExecutableProduct(prefix, "ampl", :amplexe),
 ]
 
 # Dependencies that must be installed before this package can be built
